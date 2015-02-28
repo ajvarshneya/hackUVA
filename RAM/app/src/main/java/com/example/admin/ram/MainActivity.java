61a21d250ad3a0655feca5c8e02fbbd7f7828596
Arman <arman1@vt.edu>
2015-02-28 14:14:59 -0500

    message

--- RAM/app/src/main/java/com/example/admin/ram/MainActivity.java
+++ RAM/app/src/main/java/com/example/admin/ram/MainActivity.java
@@ -36,4 +36,9 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void armanMethod()
+    {
+       //nothing
+    }
 }
