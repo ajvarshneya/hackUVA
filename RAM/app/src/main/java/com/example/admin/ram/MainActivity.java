b54d3c58a8c0e0ab1a4948e4575589f4930c7ea9
ajvarshneya <ajvarshneya@gmail.com>
2015-02-28 15:29:56 -0500

    Rejected commit: Default

--- RAM/app/src/main/java/com/example/admin/ram/MainActivity.java
+++ RAM/app/src/main/java/com/example/admin/ram/MainActivity.java
@@ -4,32 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends Activity {
 
+    ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = (ImageView) findViewById(R.id.imageview);
+        loadImage();
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -37,8 +34,9 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void armanMethod()
-    {
-       //nothing
+    // loads specified image into image view
+    public void loadImage() {
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(iv);
+        Picasso.with(this).load("http://swaha.me/images/ram1.jpg").placeholder(R.drawable.ic_launcher).into(iv);
     }
 }
