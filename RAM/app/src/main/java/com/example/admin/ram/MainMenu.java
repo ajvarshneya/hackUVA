5630b5b8851e834f51323b3863d8c1366a06dd9b
Sugat Poudel <sp5pe@virginia.edu>
2015-02-28 15:58:23 -0500

    layout changes

--- RAM/app/src/main/java/com/example/admin/ram/MainMenu.java
+++ RAM/app/src/main/java/com/example/admin/ram/MainMenu.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainMenu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
