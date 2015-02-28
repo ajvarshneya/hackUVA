4602baa7d0290297446a2b7c0293d0c1b662101e
David Zhang <xz2ed@virginia.edu>
2015-02-28 14:36:55 -0500

    NFC actvity added again?

--- RAM/app/src/main/java/com/example/admin/ram/NFC_activity.java
+++ RAM/app/src/main/java/com/example/admin/ram/NFC_activity.java
@@ -1,39 +1,49 @@
 package com.example.admin.ram;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.widget.TextView;
+import android.nfc.NfcAdapter;
+import android.widget.Toast;
+import android.app.Activity;
 
 public class NFC_activity extends ActionBarActivity {
+
+    public static final String TAG = "NfcDemo";
+
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nfc_activity);
-    }
+        setContentView(R.layout.activity_main);
 
+        mTextView = (TextView) findViewById(R.id.textView_explanation);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_nfc_activity, menu);
-        return true;
-    }
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        } else {
+            mTextView.setText(R.string.explanation);
+        }
+
+  //      handleIntent(getIntent());
     }
+
+    private void handleIntent(Intent intent) {
+        // TODO: handle Intent
+    }
+
+
 }
