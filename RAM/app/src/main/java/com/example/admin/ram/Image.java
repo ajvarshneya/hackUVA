039cfa1ed9bd94f66efa6c9acc582301e4563629
Sugat Poudel <sp5pe@virginia.edu>
2015-03-01 02:35:53 -0500

    good stuff

--- RAM/app/src/main/java/com/example/admin/ram/Image.java
+++ RAM/app/src/main/java/com/example/admin/ram/Image.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +16,8 @@ import java.util.Random;
 public class Image extends ActionBarActivity {
 
     ImageView iv;
+    int max;
+    TextView ramLabel;
 
     //int min = 1; //To give randomGenerator a range
     //int randomRamInt = randomGenerator.nextInt(4);
@@ -25,15 +28,15 @@ public class Image extends ActionBarActivity {
         setContentView(R.layout.activity_image);
         iv = (ImageView) findViewById(R.id.ram_image);
 
-        int max = getResources().getInteger(R.integer.max_rams);
+        max = getResources().getInteger(R.integer.max_rams);
 
         Random generator = new Random();
         int rand = generator.nextInt(max) + 1;
         loadImage("ram" + rand);
 
-        //TextView textView = (TextView) findViewById(R.id.tv_image);
+        ramLabel = (TextView)findViewById(R.id.tv_image);
 
-        //textView.setText("" + max);
+        ramLabel.setText("Ram " + rand);
 
     }
 
@@ -62,6 +65,13 @@ public class Image extends ActionBarActivity {
 
     // loads specified image into image view
     public void loadImage(String id) {
-        Picasso.with(this).load("http://swaha.me/images/" + id + ".jpg").fit().into(iv);
+        Picasso.with(this).load("http://swaha.me/images/" + id + ".jpg").into(iv);
+    }
+
+    public void onTouchRam(View view) {
+        Random generator = new Random();
+        int rand = generator.nextInt(max) + 1;
+        loadImage("ram" + rand);
+        ramLabel.setText("Ram " + rand);
     }
 }
